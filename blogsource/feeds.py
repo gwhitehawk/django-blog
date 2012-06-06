48d84db66e0a8bc8384f44a78c7b1d190cee8bc1
gwhitehawk <gwhitehawk@gmail.com>
2012-06-06 10:29:06 -0400

    fixed link to item

--- blogsource/feeds.py
+++ blogsource/feeds.py
@@ -2,14 +2,16 @@ from django.contrib.syndication.views import Feed
 from blogsource.models import Blog
 
 class LatestEntriesFeed(Feed):
-    title = "Latest articles"
-    link = "/latest/"
-    
+    title = "mirka's photoblog"
+    link = "/gwhitehawk/"
+    description = "the better ones of my photos"
+   
     def items(self):
         return Blog.objects.order_by('-posted')[:5]
 
     def item_title(self, item):
-        return item.title
+        result = "/gwhitehawk" + item.get_absolute_url()
+        return result
 
     def item_description(self, item):
         return item.body
