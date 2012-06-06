15b7fc29fc48f7bfa15ec7298244041731723996
gwhitehawk <gwhitehawk@gmail.com>
2012-06-05 20:34:42 -0400

    change feed address, added autodiscovery

--- blogsource/urls.py
+++ blogsource/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns('blogsource',
     (r'^$', 'views.index'),
     (r'^view/(?P<slug>[^\.]+).html', 'views.view_post'),
     (r'^category/(?P<slug>[^\.]+).html', 'views.view_category'),
-    (r'^latest/rss/$', LatestEntriesFeed()),
+    (r'^latest/feed$', LatestEntriesFeed()),
 )
